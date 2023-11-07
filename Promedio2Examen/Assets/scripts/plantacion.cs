343b0c4f282ea84e128aa8029474991f6c86bf4f
LeonardoLopez11 <leonardolopeztolentino@gmail.com>
2023-11-06 22:21:04 -0500

    creacion de las plantas y el cultivo

--- Promedio2Examen/Assets/scripts/plantacion.cs
+++ Promedio2Examen/Assets/scripts/plantacion.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class plantacion : MonoBehaviour
 {
     public GameObject plantaPrefab; 
     private bool estaPlantado = false;
+    public Slider BarradeProgreso;
+    public float tiempoCrecimiento = 1;
+
+   
+    private float tiempoTranscurrido = 0;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +23,32 @@ public class plantacion : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0) && !estaPlantado)
+        if (estaPlantado)
         {
-            Plantar();
+            tiempoTranscurrido += Time.deltaTime;
+            CargarBarraDeProgreso();
+
+            if (tiempoTranscurrido >= tiempoCrecimiento)
+            {
+                CultivarPlanta();
+            }
         }
     }
-    void Plantar()
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Jugador"))
+        {
+            if (Input.GetMouseButtonDown(0) && !estaPlantado)
+            {
+                Plantar();
+            }
+
+        }
+        
+    }
+
+    void CultivarPlanta()
     {
         
         GameObject nuevaPlanta = Instantiate(plantaPrefab, transform.position, Quaternion.identity);
@@ -28,7 +56,24 @@ public class plantacion : MonoBehaviour
         
         nuevaPlanta.transform.SetParent(transform);
 
+
+        tiempoTranscurrido = 0f;
+        estaPlantado = false;
+
         
-        estaPlantado = true;
+        BarradeProgreso.gameObject.SetActive(false);
+    }
+    void CargarBarraDeProgreso()
+    {
+        float progreso = tiempoTranscurrido / tiempoCrecimiento;
+        BarradeProgreso.value = progreso;
+    }
+    public void Plantar()
+    {
+        if (!estaPlantado)
+        {
+            estaPlantado = true;
+            BarradeProgreso.gameObject.SetActive(true);
+        }
     }
 }
